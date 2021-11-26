ef84a364c2209ad1899b525fbe3b4e002574cdf0
apavankumar11 <73984128+apavankumar11@users.noreply.github.com>
2021-11-26 10:57:50 +0530

    Add files via upload
    add parametrized

--- testsuites/test_bank.py
+++ testsuites/test_bank.py
@@ -1,6 +1,8 @@
 import pytest
 from bankoperations import withdraw, balance
+from operations import *
 
+'''
 @pytest.fixture()
 def check():
     print('test about to start')
@@ -9,4 +11,15 @@ def check():
 
 def test_withdraw(check):
    assert withdraw(3000) == 0
+'''
 
+
+#Parametrize test cases
+
+@pytest.mark.parametrize('x,y,ans',[(10,20,30),(40,50,90)])
+def test_add(x,y,ans):
+    assert add(x,y) == ans
+
+@pytest.mark.parametrize('amount,ans',[(2000,1000),(500,500)])
+def test_withdraw(amount,ans):
+    assert withdraw(amount) == ans
